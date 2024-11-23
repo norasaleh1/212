079b1ebb29422d3004f810d2bb5ff41f361c0ecf
norasaleh1 <444201090@student.ksu.edu.sa>
2024-11-23 21:35:52 +0300

    Add files via upload

---
+++ doc.java
@@ -0,0 +1,14 @@
+//impot LinkedList
+public class doc {
+	LinkedList<String>word=new LinkedList<>();
+	String text ;
+	int id ;
+	
+	doc (int id ,LinkedList<String>word , String text){
+		this.id=id;
+		this.word=word;
+		this.text=text ;
+	}
+		
+
+}
